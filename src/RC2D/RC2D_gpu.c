38d4884564bc62271da66b41fb028562aaa33049
Corentin Recanzone <corentindevpro@hotmail.com>
2025-04-20 00:55:03 +0200

    m

--- src/RC2D/RC2D_gpu.c
+++ src/RC2D/RC2D_gpu.c
@@ -4,19 +4,31 @@
 
 #include <SDL3/SDL_properties.h>
 
-void rc2d_gpu_getInfo(RC2D_GPUDevice* gpuDevice, RC2D_GPUInfo* gpuInfo) {
+void rc2d_gpu_getInfo(RC2D_GPUDevice* gpuDevice, RC2D_GPUInfo* gpuInfo) 
+{
+    // Vérification des paramètres d'entrée
     RC2D_assert_release(gpuDevice != NULL, RC2D_LOG_CRITICAL, "GPU device is NULL.");
     RC2D_assert_release(gpuInfo != NULL, RC2D_LOG_CRITICAL, "GPU info is NULL.");
 
+    // Récupération des propriétés du GPU
     SDL_PropertiesID propsGPU = SDL_GetGPUDeviceProperties(gpuDevice);
 
+    // Vérification de la récupération des propriétés
     RC2D_assert_release(propsGPU != 0, RC2D_LOG_CRITICAL, "Failed to get GPU properties : %s", SDL_GetError());
 
+    // Récupération des informations sur le GPU
     gpuInfo->gpu_device_name = SDL_GetStringProperty(propsGPU, SDL_PROP_GPU_DEVICE_NAME_STRING);
     gpuInfo->gpu_device_driver_name = SDL_GetStringProperty(propsGPU, SDL_PROP_GPU_DEVICE_DRIVER_NAME_STRING);
     gpuInfo->gpu_device_driver_version = SDL_GetStringProperty(propsGPU, SDL_PROP_GPU_DEVICE_DRIVER_VERSION_STRING);
     gpuInfo->gpu_device_driver_info = SDL_GetStringProperty(propsGPU, SDL_PROP_GPU_DEVICE_DRIVER_INFO_STRING);
 
+    // Logs des informations sur le GPU
+    RC2D_log(RC2D_LOG_INFO, "GPU device name: %s", gpuInfo->gpu_device_name);
+    RC2D_log(RC2D_LOG_INFO, "GPU device driver name: %s", gpuInfo->gpu_device_driver_name);
+    RC2D_log(RC2D_LOG_INFO, "GPU device driver version: %s", gpuInfo->gpu_device_driver_version);
+    RC2D_log(RC2D_LOG_INFO, "GPU device driver info: %s", gpuInfo->gpu_device_driver_info);
+
+    // Destruction des propriétés du GPU
     SDL_DestroyProperties(propsGPU);
 }
 
