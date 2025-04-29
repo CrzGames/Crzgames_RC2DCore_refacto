361b607daf0446e42f6160bebaf0400c8ab1d756
Corentin Recanzone <corentindevpro@hotmail.com>
2025-04-29 16:26:57 +0200

    m

--- include/rc2d/RC2D_config.h
+++ include/rc2d/RC2D_config.h
@@ -9,38 +9,39 @@ extern "C" {
 /**
  * \brief Si RC2D_SHADER_HOT_RELOAD_ENABLED est défini à 1, le support des shaders en ligne est activé.
  * 
- * Cette option permet d'activer le rechargement à chaud des shaders, ce qui signifie que les shaders 
- * peuvent être rechargés sans avoir à redémarrer l'application. Permet lors de la modification des source d'un shader HLSL de
+ * Cette option permet d'activer le rechargement à chaud des shaders, ce qui signifie que les shaders peuvent être 
+ * rechargés sans avoir à redémarrer l'application. Permet lors de la modification des source d'un shader HLSL de
  * voir le résultat immédiatement en cours d'exécution dans l'application.
  * 
  * \note Cette fonctionnalité est utile pour le développement, mais peut avoir un impact sur les performances.
- * Il est donc recommandé de la désactiver pour les versions de production et d'utiliser les shaders hors ligne (précompilés).
+ * Il est donc fortement recommandé de la désactiver pour les versions de production et d'utiliser les shaders hors ligne (précompilés).
+ * Ce qui évitera également d'embarquer les bibliothèques tiers dans l'application.
  * 
  * Pour Windows, il faudra linker/fournir les bibliothèques suivantes :
  * - dxcompiler.dll : Traduit HLSL en DXIL (Direct3D12) ou SPIR-V (Vulkan).
  * - dxil.dll : Contient les définitions de base et le validateur DXIL officiel de Microsoft.
- * - spirv-cross-c-shared.dll : Traduit SPIR-V en MSL (Metal) + Permet les réflexions automatiques sur les ressources shaders.
+ * - spirv-cross-c-shared.dll : Permet les réflexions automatiques sur les ressources shaders.
  * - SDL3_shadercross.dll + SDL3_shadercross.lib OU SDL3_shadercross_static.lib : Permet la compilation en ligne de shaders.
- * - SDL3.dll : Bibliothèque runtime SDL3.
+ * 
+ * \note Pour Windows, SPIRV-Cross ne permet pas de traduire les shaders SPIR-V en MSL (Metal), mais permet quand même d'avoir 
+ * les réflexions automatiques sur les ressources shaders.
  * 
  * Pour Linux, il faudra linker/fournir les bibliothèques suivantes :
- * - libdxcompiler.so : Traduit HLSL en SPIR-V ou DXIL.
+ * - libdxcompiler.so : Traduit HLSL en DXIL (Direct3D12) ou SPIR-V (Vulkan).
  * - libdxil.so : Contient les définitions de base et la validation du format DXIL.
  * - libspirv-cross-c-shared.so : Traduit SPIR-V en MSL (Metal) + Permet la réflexion automatique des ressources shaders.
- * - libSDL3.so.0 : Bibliothèque runtime SDL3.
  * - libSDL3_shadercross.so : Permet la compilation en ligne de shaders.
- * - libvkd3d.so : Composant principal pour la conversion HLSL vers SPIR-V.
- * - libvkd3d-shader.so : Bibliothèque de traduction des shaders (HLSL -> SPIR-V).
+ * - libvkd3d.so : ?
+ * - libvkd3d-shader.so : ?
  * - libvkd3d-utils.so : Bibliothèque utilitaire pour VKD3D.
  * 
  * Pour macOS, il faudra linker/fournir les bibliothèques suivantes :
- * - libdxcompiler.dylib : Traduit HLSL en SPIR-V ou DXIL.
+ * - libdxcompiler.dylib : Traduit HLSL en DXIL (Direct3D12) ou SPIR-V (Vulkan).
  * - libdxil.dylib : Contient les définitions de base et la validation du format DXIL.
  * - libspirv-cross-c-shared.dylib : Traduit SPIR-V en MSL (Metal) + Permet la réflexion automatique des ressources shaders.
- * - libSDL3.0.dylib : Bibliothèque runtime SDL3.
  * - libSDL3_shadercross.dylib : Permet la compilation en ligne de shaders.
- * - libvkd3d.dylib : Composant principal pour la conversion HLSL vers SPIR-V.
- * - libvkd3d-shader.dylib : Bibliothèque de traduction des shaders (HLSL -> SPIR-V).
+ * - libvkd3d.dylib : ?
+ * - libvkd3d-shader.dylib : ?
  * - libvkd3d-utils.dylib : Bibliothèque utilitaire pour VKD3D.
  */
 #ifndef RC2D_SHADER_HOT_RELOAD_ENABLED
