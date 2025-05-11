a29cac2ce06dc4e8303048f577a1100d5cab7b2e
Corentin Recanzone <corentindevpro@hotmail.com>
2025-05-11 21:20:14 +0200

    m

--- include/rc2d/RC2D_config.h
+++ include/rc2d/RC2D_config.h
@@ -32,6 +32,44 @@ extern "C" {
 #define RC2D_GPU_SHADER_HOT_RELOAD_ENABLED 0
 #endif
 
+/**
+ * \brief Si RC2D_NET_MODULE_ENABLED est défini à 1, le support du réseau (UDP) est activé.
+ * 
+ * Cela utilise la bibliothèque RCENet pour la gestion des sockets et des connexions réseau.
+ * RCENet est une bibliothèque de réseau multiplateforme qui permet de créer des applications réseau
+ * en C/C++. Elle fournit une API simple et efficace pour la gestion des connexions réseau, 
+ * la communication entre les clients et les serveurs, ainsi que la gestion des événements réseau.
+ * 
+ * \note RCENet est un fork de ENet, qui est une bibliothèque de réseau populaire pour les jeux vidéo.
+ */
+#ifndef RC2D_NET_MODULE_ENABLED
+#define RC2D_NET_MODULE_ENABLED 1
+#endif
+
+/**
+ * \brief Si RC2D_DATA_MODULE_ENABLED est défini à 1, la compression / chiffrement / hachage est activé.
+ * 
+ * Cela utilise la bibliothèque OpenSSL pour la gestion des algorithmes de chiffrement et de hachage.
+ * OpenSSL est une bibliothèque open-source qui fournit des outils pour la sécurité des communications
+ * sur les réseaux informatiques. Elle est largement utilisée pour le chiffrement, la signature numérique,
+ * la gestion des certificats et d'autres tâches liées à la sécurité.
+ */
+#ifndef RC2D_DATA_MODULE_ENABLED
+#define RC2D_DATA_MODULE_ENABLED 1
+#endif
+
+/**
+ * \brief Si RC2D_ONNX_MODULE_ENABLED est défini à 1, le support de ONNX est activé.
+ * 
+ * Cela utilise la bibliothèque ONNX Runtime pour la gestion des modèles ONNX.
+ * ONNX Runtime est un moteur d'exécution haute performance pour les modèles de machine learning
+ * au format Open Neural Network Exchange (ONNX). Il permet d'exécuter des modèles ONNX sur différentes
+ * plateformes et matériels, optimisant ainsi les performances et la portabilité des modèles de machine learning.
+ */
+#ifndef RC2D_ONNX_MODULE_ENABLED
+#define RC2D_ONNX_MODULE_ENABLED 1
+#endif
+
 /* Termine les définitions de fonctions C lors de l'utilisation de C++ */
 #ifdef __cplusplus
 }
