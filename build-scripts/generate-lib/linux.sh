3432344842146ba94c3dfa7904ac0cef43b6c6ca
Corentin Recanzone <corentindevpro@hotmail.com>
2025-05-21 18:50:00 +0200

    m

--- build-scripts/generate-lib/linux.sh
+++ build-scripts/generate-lib/linux.sh
@@ -27,21 +27,5 @@ cmake ../../../../.. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYST
 echo -e "\e[32m \n Build project for Release...\e[0m"
 cmake --build .
 
-# Return to the base directory
-cd ../../../../..
-
-cp -r dist/lib/linux/x86_64/Debug/*.a dist/lib/linux/outputs/x86_64/Debug
-cp -r dist/lib/linux/x86_64/Release/*.a dist/lib/linux/outputs/x86_64/Release
-
-rm -rf dist/lib/linux/x86_64
-
-mkdir -p dist/lib/linux/Release/x86_64
-mkdir -p dist/lib/linux/Debug/x86_64
-
-cp -r dist/lib/linux/outputs/x86_64/Release/*.a dist/lib/linux/Release/x86_64
-cp -r dist/lib/linux/outputs/x86_64/Debug/*.a dist/lib/linux/Debug/x86_64
-
-rm -rf dist/lib/linux/outputs
-
 # Final message
 echo -e "\033[32m \n Generate lib RC2D for Linux x86_64 to Release/Debug generated successfully, go to the dist/lib/linux/x86_64 directory... \n\033[0m"
