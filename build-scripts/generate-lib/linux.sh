d7a836c27acfc1bc20525a5014909211fdae3e8a
Corentin Recanzone <corentindevpro@hotmail.com>
2025-05-21 19:45:05 +0200

    m

--- build-scripts/generate-lib/linux.sh
+++ build-scripts/generate-lib/linux.sh
@@ -2,29 +2,12 @@
 
 # Generate lib for Linux to x86_64 architecture
 
-mkdir -p dist/lib/linux/x86_64
-mkdir -p dist/lib/linux/outputs/x86_64/Debug
-mkdir -p dist/lib/linux/outputs/x86_64/Release
-
-# Build for Debug
-mkdir -p dist/lib/linux/x86_64/Debug
-cd dist/lib/linux/x86_64/Debug
+mkdir -p build/linux/x86_64
+cd build/linux/x86_64
 
 echo -e "\e[32m \n Generate project with CMake for Linux x86_64...\e[0m"
-cmake ../../../../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_NAME=Linux
+cmake ../../.. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_NAME=Linux
 echo -e "\e[32m \n Build project for Debug...\e[0m"
-cmake --build .
-
-# Return to the base directory
-cd ../../../../..
-
-# Build for Release
-mkdir -p dist/lib/linux/x86_64/Release
-cd dist/lib/linux/x86_64/Release
-
-echo -e "\e[32m \n Generate project with CMake for Linux x86_64...\e[0m"
-cmake ../../../../.. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Linux 
-echo -e "\e[32m \n Build project for Release...\e[0m"
 cmake --build .
 
 # Final message
